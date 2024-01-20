f115ed121c72db2809b7def0417ad95cab0e1fb5
Ruchika Jadhav <jadhavruchika3@gmail.com>
2024-01-20 20:55:15 +0530

    Add files via upload

---
+++ includes/database_connect.php
@@ -0,0 +1,9 @@
+<?php
+$conn = mysqli_connect("localhost:3306", "root", "", "pglife");
+
+if (mysqli_connect_errno()) {
+    // Throw error message based on ajax or not
+    echo "Failed to connect to MySQL! Please contact the admin.";
+    return;
+}
+?>
